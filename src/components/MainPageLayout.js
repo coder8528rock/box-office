aa664f06645c6eeab6eeb3f7cadea10cc732a34e
Abhishek <ap5219084@gmail.com>
2022-06-19 16:02:16 +0530

    added layout management

---
+++ src/components/MainPageLayout.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import Navs from './Navs';
+import Title from './Title';
+
+const MainPageLayout = ({ children }) => {
+  return (
+    <div>
+      <Title title="Box-Office" subtitle="Are you looking for an actor?" />
+      <Navs />
+      {children}
+    </div>
+  );
+};
+
+export default MainPageLayout;
