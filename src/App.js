93b17bdf6192bac00d06fa148878b630810ea118
Abhishek <ap5219084@gmail.com>
2022-06-24 13:17:50 +0530

    added show pages and added id error in showGrid.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/self-closing-comp */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Show from './pages/Show';
 import Starred from './pages/Starred';
 
 function App() {
@@ -12,6 +14,9 @@ function App() {
       </Route>
       <Route exact path="/starred">
         <Starred />
+      </Route>
+      <Route exact path="/show/:id">
+        <Show />
       </Route>
       <Route>
         <div>this page not found</div>
